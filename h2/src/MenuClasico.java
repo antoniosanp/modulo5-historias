b9aa5dc05e9d28ef8e28e2ccfe24cba30d982845
antoniosanp <antonio.pulgarin97@gmail.com>
2026-03-29 18:54:30 -0500

    switch de java 8

---
+++ h2/src/MenuClasico.java
@@ -0,0 +1,23 @@
+public class MenuClasico {
+     public static void main(String[] args) {
+        int opcion = 3;
+        switch (opcion) {
+            case 1:
+                System.out.println("Opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+                break;
+            case 3:
+                System.out.println("Opcion 3");
+                break;
+            case 4:
+                System.out.println("Opcion 4");
+                break;
+        
+            default:
+                System.out.println("opcion inválida");
+                break;
+        }
+    }
+}
---
+++ h2/src/MenuModerno.java
@@ -0,0 +1,25 @@
+public class MenuModerno {
+    
+    public static void main(String[] args) {
+        int opcion = 3;
+        switch (opcion) {
+            case 1:
+                System.out.println("Opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+                break;
+            case 3:
+                System.out.println("Opcion 3");
+                break;
+            case 4:
+                System.out.println("Opcion 4");
+                break;
+        
+            default:
+                System.out.println("opcion inválida");
+                break;
+        }
+    }
+
+}
