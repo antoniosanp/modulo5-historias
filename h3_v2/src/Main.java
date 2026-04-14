d3348e7de2809240a6c7e3289cfffc77891d7ea5
antoniosanp <antonio.pulgarin97@gmail.com>
2026-04-13 20:21:42 -0500

    refacto Main

--- h3_v2/src/Main.java
+++ h3_v2/src/Main.java
@@ -1,3 +1,25 @@
+import models.Empleado;
+import service.EmpleadoService;
 public class Main {
-    
+
+
+
+    public static void main(String[] args) {
+    Empleado empleado1 = new Empleado(1, "pedro el empleado");
+    Empleado empleado2 = new Empleado(2, "sofia trabajadora");
+    Empleado empleado3 = new Empleado(3, "juan mecanico");
+    Empleado empleado4 = new Empleado(4, "federico laburando");
+
+    EmpleadoService empleadoService = new EmpleadoService();
+
+    empleadoService.addEmpleado(empleado1);
+    empleadoService.addEmpleado(empleado2);
+    empleadoService.addEmpleado(empleado3);
+    empleadoService.addEmpleado(empleado3);
+    empleadoService.addEmpleado(empleado4);
+
+    empleadoService.showEmpleados();
+
+
+    }
 }
