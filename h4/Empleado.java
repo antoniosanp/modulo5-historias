b47c7b9948e5ea05b6b77415d924edb9fe2089ab
antoniosanp <antonio.pulgarin97@gmail.com>
2026-04-13 20:50:32 -0500

    fix: Empleado

--- h4/Empleado.java
+++ h4/Empleado.java
@@ -1,24 +1,26 @@
 package h4;
 public class Empleado {
     byte age;
-    short yearsOfExperience;
+    short idSede;
     int idEmpleado;
     char departamento;
     long bonoMensual;
     double salario;
     String nombre;
     boolean esActivo;
+    float puntajeTest;
 
-    public Empleado(byte age, short yearsOfExperience, int idEmpleado, char departamento, long bonoMensual, double sueldo,
-            String nombre, boolean esActico) {
+    public Empleado(byte age, short idSede, int idEmpleado, char departamento, long bonoMensual, double sueldo,
+            String nombre, boolean esActico, float puntajeTest) {
         this.age = age;
-        this.yearsOfExperience = yearsOfExperience;
+        this.idSede = idSede;
         this.idEmpleado = idEmpleado;
         this.departamento = departamento;
         this.bonoMensual = bonoMensual;
         this.salario = sueldo;
         this.nombre = nombre;
         this.esActivo = esActico;
+        this.puntajeTest = puntajeTest;
     }
 
     public double calcularSalarioFinal(){
@@ -27,7 +29,10 @@ public class Empleado {
 
         return (salario + bonoAdd*1.10);
 
+    }
 
+    public boolean validarElegibilidad(){
+        return (puntajeTest > 85 && age < 30) || (idSede == 1 && !esActivo);
     }
 
     
